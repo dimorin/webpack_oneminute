a6056ffe79cf8059d840e5c23d60a49837018839
dimorin <dimorinpk@gmail.com>
2022-04-25 23:29:59 +0900

    install axios

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,11 @@
 import 'bootstrap';
+import axios from 'axios';
 import {message} from './message';
 import '../css/main.scss';
 
 message('1분코딩 웹팩 간단 설정');
+
+axios({
+    method: 'get',
+    url: 'https://jsonplaceholder.typicode.com/todos/1',
+  }).then(response => alert(JSON.stringify(response.data)));
